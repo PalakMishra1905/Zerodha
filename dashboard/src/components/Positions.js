61a1e151c7b03c543d732133f9959f15f191b1f4
Palak Mishra <mishrapalak104@gmail.com>
2026-02-18 20:12:05 +0530

    Update API endpoint for fetching all positions

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -6,7 +6,7 @@ const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
 
   useEffect(()=>{
-     axios.get('http://localhost:3002/allPositions').then((res)=>{
+     axios.get('https://zerodha-backend-2-53zw.onrender.com/allPositions').then((res)=>{
         console.log(res.data);
         setAllPositions(res.data);
      });
